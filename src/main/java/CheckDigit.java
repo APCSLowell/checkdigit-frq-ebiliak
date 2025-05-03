8e2b338d56371111c7fa24130eea7626a3cab48b
ebiliak <ilforoughi@s.sfusd.edu>
2025-05-02 21:54:21 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -1,10 +1,9 @@
 public class CheckDigit 
 {   
-  /** Returns the check digit for num, as described in part (a).  
-   *  Precondition: The number of digits in num is between one and    
-   *  six, inclusive.  
-   *          num >= 0  
-   */  
+/** Returns the check digit for num, as described in part (a).  
+ *  Precondition: The number of digits in num is between one and six, inclusive.  
+ *                num >= 0  
+ */
    public static int getCheck(int num) 
    {  
      int sum = 0;
